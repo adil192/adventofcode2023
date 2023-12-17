10abfe695414120c83bc721b246cc5f1c84ca1e4
Adil Hanney <adilh+github@orchid.anonaddy.com>
2023-12-17 21:28:12 +0000

    Initial commit

---
+++ bin/adventofcode2023.dart
@@ -0,0 +1,5 @@
+import 'package:adventofcode2023/adventofcode2023.dart' as adventofcode2023;
+
+void main(List<String> arguments) {
+  print('Hello world: ${adventofcode2023.calculate()}!');
+}
---
+++ test/adventofcode2023_test.dart
@@ -0,0 +1,8 @@
+import 'package:adventofcode2023/adventofcode2023.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
