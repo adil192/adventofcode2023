b76417aec25963b4ae600eb52ee32d3ba4d37728
Adil Hanney <adilh+github@orchid.anonaddy.com>
2023-12-17 22:05:54 +0000

    Day 1 Part 2

--- lib/week_1.dart
+++ lib/week_1.dart
@@ -1,5 +1,23 @@
 import 'dart:io';
 
+/// A list of words that can be used to represent numbers.
+/// E.g. numberWords[1] == 'one'
+///
+/// Note that the challenge doesn't accept 'zero' as a number,
+/// but it's included to keep the indices consistent.
+const numberWords = [
+  'zero',
+  'one',
+  'two',
+  'three',
+  'four',
+  'five',
+  'six',
+  'seven',
+  'eight',
+  'nine',
+];
+
 /// Gets the first and last digit in the line
 /// and concatenates them.
 /// e.g. ab1cd3ef => 13
@@ -8,14 +26,70 @@ int getCalibrationValue(String line) {
 
   final chars = line.split('');
 
-  String firstDigit = chars.firstWhere((c) => isDigit(c));
-  String lastDigit = chars.lastWhere((c) => isDigit(c));
+  String? firstDigit;
+  String? lastDigit;
+
+  // forward search
+  for (int i = 0; i < chars.length; i++) {
+    final digit = getNumberAtPos(line, i);
+    if (digit != null) {
+      firstDigit = digit.toString();
+      break;
+    }
+  }
+  // backward search
+  for (int i = chars.length - 1; i >= 0; i--) {
+    final digit = getNumberAtPos(line, i);
+    if (digit != null) {
+      lastDigit = digit.toString();
+      break;
+    }
+  }
+
+  if (firstDigit == null || lastDigit == null) {
+    throw ArgumentError.value(line, 'line', 'No digits found');
+  }
 
   return int.parse('$firstDigit$lastDigit');
 }
 
-bool isDigit(String c) {
-  return c.codeUnitAt(0) >= 48 && c.codeUnitAt(0) <= 57;
+/// Checks if [line] contains a number at [index], and returns it.
+/// Returns null if no number is found.
+///
+/// The number can also be spelt out, e.g. "one" or "two".
+int? getNumberAtPos(String line, int index) {
+  // first check if it's a digit character
+  final c = line[index];
+  if (c.codeUnitAt(0) >= 48 && c.codeUnitAt(0) <= 57) {
+    return int.parse(c);
+  }
+
+  // then check if it's a word
+  return _isWordAtPos(line, index);
+}
+
+/// Checks if a number is spelled out beginning at [index] in [line].
+int? _isWordAtPos(String line, int index) {
+  final possibilities = numberWords.toList();
+  possibilities.removeAt(0); // remove 'zero'
+
+  // (there are max 5 letters in a number word)
+  for (int c = 0; c < 5; c++) {
+    for (int w = 0; w < possibilities.length; w++) {
+      final word = possibilities[w];
+      if (line.length <= index + c ||
+          word.length <= c ||
+          line[index + c] != word[c]) {
+        possibilities.removeAt(w);
+        w--;
+      } else if (word.length == c + 1) {
+        // word is complete
+        return numberWords.indexOf(word);
+      }
+    }
+  }
+  assert(possibilities.isEmpty);
+  return null;
 }
 
 Future<int> main() async {
--- test/week_1_test.dart
+++ test/week_1_test.dart
@@ -4,24 +4,26 @@ import 'package:test/test.dart';
 final _egLines = '''1abc2
 pqr3stu8vwx
 a1b2c3d4e5f
-treb7uchet'''
+treb7uchet
+xyzone23456seven'''
     .split('\n');
 
 void main() {
   group('Week 1', () {
+    test('getNumberAtPos', () {
+      expect(getNumberAtPos(_egLines[0], 0), 1);
+      expect(getNumberAtPos(_egLines[0], 1), null);
+      expect(getNumberAtPos(_egLines[4], 3), 1);
+      expect(getNumberAtPos(_egLines[4], 4), null);
+      expect(getNumberAtPos(_egLines[4], 6), 2);
+      expect(getNumberAtPos(_egLines[4], 11), 7);
+    });
     test('getCalibrationValue', () {
       expect(getCalibrationValue(_egLines[0]), 12);
       expect(getCalibrationValue(_egLines[1]), 38);
       expect(getCalibrationValue(_egLines[2]), 15);
       expect(getCalibrationValue(_egLines[3]), 77);
-    });
-    test('isDigit', () {
-      expect(isDigit('0'), true);
-      expect(isDigit('9'), true);
-      expect(isDigit('a'), false);
-      expect(isDigit('z'), false);
-      expect(isDigit('A'), false);
-      expect(isDigit('Z'), false);
+      expect(getCalibrationValue(_egLines[4]), 17);
     });
   });
 }
