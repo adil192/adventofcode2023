e75475a9326b5792bd76b7c2c48923592daa2028
Adil Hanney <adilh+github@orchid.anonaddy.com>
2023-12-18 19:38:58 +0000

    Day 2 Part 2

--- lib/day_2.dart
+++ lib/day_2.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'dart:math';
 
 /// Holds the count of [r] red cubes, [g] green cubes, and [b] blue cubes.
 ///
@@ -64,15 +65,31 @@ bool possibleGame(List<Round> rounds, Round maxPerRound) =>
         round.g <= maxPerRound.g &&
         round.b <= maxPerRound.b);
 
+/// (Part 2) Gets the fewest number of red, green, and blue cubes
+/// that could have been in the bag for [rounds] to be possible.
+Round minPossibleCubes(List<Round> rounds) => (
+      r: rounds.map((round) => round.r).reduce(max),
+      g: rounds.map((round) => round.g).reduce(max),
+      b: rounds.map((round) => round.b).reduce(max),
+    );
+
+/// (Part 2) Gets the power of a round,
+/// which is the product of the number of cubes of each color.
+int powerOfRound(Round round) => round.r * round.g * round.b;
+
 /// Returns the sum of the IDs of the games that would have been possible
 /// if the bag had been loaded with only 12 red cubes, 13 green cubes,
 /// and 14 blue cubes.
-Future<int> main() async {
+Future<void> main() async {
   const maxPerRound = (r: 12, g: 13, b: 14);
   final lines = await File('assets/day_2.txt').readAsLines();
   final games = parseInput(lines);
-  final possible = possibleGames(games, maxPerRound);
-  final sum = possible.reduce((a, b) => a + b);
-  print('The sum of the IDs of the possible games is $sum');
-  return sum;
+
+  final possibleIds = possibleGames(games, maxPerRound);
+  final sumPossibleIds = possibleIds.reduce((a, b) => a + b);
+  print('The sum of the IDs of the possible games is $sumPossibleIds');
+
+  final roundPowers = games.values.map(minPossibleCubes).map(powerOfRound);
+  final sumRoundPowers = roundPowers.reduce((a, b) => a + b);
+  print('The sum of the powers of the minimum set of cubes is $sumRoundPowers');
 }
